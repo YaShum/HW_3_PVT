8b2c707d7f6b41aa5f58575cab99000a7a66cdc9
YaShum <yyarmak@gmail.com>
2017-05-31 10:59:24 +0300

    Update GameAnimation.js

--- GameAnimation.js
+++ GameAnimation.js
@@ -1,11 +1,5 @@
 var gameP = document.getElementById('gameHandPetja');
-for (var k = 0; k < 18;) {
-    gameP.innerHTML = cardPetja[k++] + gameP.innerHTML;
-}
 var gameV = document.getElementById('gameHandVasja');
-for (var s = 0; s < 18;) {
-    gameV.innerHTML = cardVasja[s++] + gameV.innerHTML;
-}
 
 game.onclick = function () {
     document.getElementById('but').style.display = "none";
@@ -20,25 +14,24 @@ game.onclick = function () {
         suitCard.classList.add('animate_suit');
         document.getElementById('suitCard').innerHTML = arrSuit[randNum];
         
+        gameP.innerHTML = cardPetja[k++] + gameP.innerHTML;
+        gameV.innerHTML = cardVasja[s++] + gameV.innerHTML;
+        
+         var timersFr = setTimeout(function func1(){              
+                var elmP = gameP.childNodes[t++];              
+                elmP.className = "animate_gameHandP";
+
+                var elmV = gameV.childNodes[r++];
+                elmV.className = "animate_gameHandV";
+              
+            }, 1000);
+        
         var elmP = gameP.childNodes[t++];
         elmP.className = "animate_gameHandP";
         
         var elmV = gameV.childNodes[r++];
         elmV.className = "animate_gameHandV";
 
-        /*var gameP = document.getElementById('gameHandPetja');
-        gameP.innerHTML = cardPetja[k++] + gameP.innerHTML;
-
-        var gameV = document.getElementById('gameHandVasja');     
-        gameV.innerHTML = cardVasja[s++] + gameV.innerHTML;
-        
-        var elmV = gameV.querySelector("li");
-        elmV.className = "animate_gameHandV";
-
-        var elmP = gameP.querySelector("li");
-        elmP.className = "animate_gameHandP";*/
-        
-        
         var pointP = document.getElementById('gamePointsPetja');
         pointP.innerHTML = numPetja[l++];
 
